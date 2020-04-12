dd4c2b37d5814a10b7b7cda85b4cb3a4e31144a5
Nikita Sakun <NikitaSakun1@gmail.com>
2020-04-12 16:46:08 +0300

    Second commit I rebuild my code hash table

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,6 @@
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        QuadraticProbingHashTable table = generator(20000);
+          QuadraticProbingHashTable hashTable = new QuadraticProbingHashTable(100);
     }
-    private static QuadraticProbingHashTable generator(int numberofelements) {
-        QuadraticProbingHashTable structure = new QuadraticProbingHashTable(numberofelements);
-        Random random = new Random();
-        for (int i = 0; i < numberofelements; i++) {
-            StringBuilder key = new StringBuilder();
-            for (int j = 0; j < random.nextInt(20); j++) {
-                int choice = random.nextInt(('9' - '0' + 1) + ('Z' - 'A' + 1));
-                key.append(((char) (choice < 10 ? '0' + choice : choice - 10 + 'A')));
-            }
-            structure.insert(key.toString(), random.nextInt() + "");
-        }
-        return structure;
-    }
+
 }
--- src/QuadraticProbingHashTable.java
+++ src/QuadraticProbingHashTable.java
@@ -1,35 +1,41 @@
 public class QuadraticProbingHashTable {
+    //текущий размер для вывода текущего заполнения матрицы и максимальный размер для установки размера массивов ключей и значений
     private int currentSize, maxSize;
+    //кон
+    private static final int hash32Init = 0x811c9dc5;
+    private static final int hash32Prime = 0x01000193;
+    //ключ по которому мы будем искать значение
     private String[] keys;
+    //само значение
     private String[] vals;
-
-    /** Constructor **/
+    //конструктор
     public QuadraticProbingHashTable(int capacity)
     {
-        currentSize = 0;
-        maxSize = capacity;
-        keys = new String[maxSize];
-        vals = new String[maxSize];
+        this.currentSize = 0;
+        this.maxSize = capacity;
+        this.keys = new String[maxSize];
+        this.vals = new String[maxSize];
     }
+    //получаем колличество элементов в таблице
     public int getSize()
     {
         return currentSize;
     }
-    public boolean isFull()
+    //хеш функция FNV32
+    private int hash32(String key)
     {
-        return currentSize == maxSize;
+        int rv = hash32Init;
+        final int len = key.length();
+        for(int i = 0; i < len; i++) {
+            rv ^= key.charAt(i);
+            rv *= hash32Prime;
+        }
+        return rv;
     }
-    public boolean contains(String key)
-    {
-        return get(key) !=  null;
-    }
-    private int hash(String key)
-    {
-        return key.hashCode() % maxSize;
-    }
+    //метод всавки
     public void insert(String key, String val)
     {
-        int tmp = Math.abs(hash(key));
+        int tmp = hash32(key);
         int i = tmp, h = 1;
         do
         {
@@ -48,9 +54,10 @@ public class QuadraticProbingHashTable {
             i = (i + h * h++) % maxSize;
         } while (i != tmp);
     }
+    //метод поиска элемента
     public String get(String key)
     {
-        int i = hash(key), h = 1;
+        int i = hash32(key), h = 1;
         while (keys[i] != null)
         {
             if (keys[i].equals(key))
@@ -60,4 +67,5 @@ public class QuadraticProbingHashTable {
         }
         return null;
     }
+
 }
